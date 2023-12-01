2712f7e28b63ba45b92f961ea41a223e12e2afea
Mikhail <aksenov.m87@yandex.ru>
2023-12-02 00:50:31 +0300

    feat: добавить дату заказа

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,17 +1,65 @@
 <script setup>
 const props = defineProps(["content", "class"]);
+
+function formatDate(inputDate) {
+  // Преобразуем строку в объект Date
+  let dateObj = new Date(inputDate);
+
+  // Получаем месяц в числовом формате (от 0 до 11)
+  let month = dateObj.getMonth();
+
+  // Массив с названиями месяцев
+  let monthNames = [
+    "янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"
+  ];
+
+  // Форматируем дату
+  let formattedDate = (month + 1) + "." + monthNames[month];
+
+  return formattedDate;
+}
+
+
 </script>
 
 <template>
-  <li class="cell" :class="props.class">
+  <ul class="cell__column" v-if="props.class === 'tariff'">
+    <li v-for="(value, index) in props.content" :key="index">
+      <p class="text">{{ value }}</p>
+    </li>
+  </ul>
+  <ul class="cell__column_diets" v-else-if="props.class === 'diets'">
+    <li v-for="(value, index) in props.content" :key="index">
+      <p class="text">{{ value }}</p>
+    </li>
+  </ul>
+  <!-- <ul
+    class="cell__column_pay"
+    v-else-if="props.class === 'order_payed'"
+  >
+    <li class="cell">
+      <p class="text">{{ orderDetails.order_payed }}</p>
+    </li>
+    <li class="cell">
+      <p class="text">{{ orderDetails.order_payed }}</p>
+    </li>
+    <li class="cell">
+      <p class="text">{{ orderDetails.discount }}</p>
+    </li>
+  </ul> -->
+  <ul class="cell" :class="props.class" v-else-if="props.class === 'dates'">
+    <li v-for="(value, index) in props.content" :key="index">
+      <p class="text">{{ formatDate(value.start_date) + ' - ' + formatDate(value.end_date) }}</p>
+    </li>
+  </ul>
+  <li v-else class="cell" :class="props.class">
     <p class="text">{{ props.content }}</p>
   </li>
 </template>
 
-<style scoped>
-
+<style>
 .cell {
-    background-color: rgba(255, 255, 255, 1);
+  background-color: rgba(255, 255, 255, 1);
 }
 .text {
   font-weight: 400;
@@ -19,9 +67,13 @@ const props = defineProps(["content", "class"]);
   line-height: 16px;
   color: #000000;
 }
-.client_name p, 
-.o_id p 
-{
-color: blue;
+.client_name p,
+.o_id p {
+  color: blue;
+}
+
+.cell__column_pay {
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/components/Row.vue
+++ src/components/Row.vue
@@ -1,10 +1,68 @@
 <script setup>
 import Cell from "./Cell.vue";
 const props = defineProps(["data"]);
+
+const remainingTime = (date) => {
+  // Преобразуем строку начальной даты в объект Date
+  let startDate = new Date(date);
+  // Получаем текущую дату
+  let currentDate = new Date();
+  // Разница в миллисекундах
+  let timeDifference = currentDate - startDate;
+  // Преобразуем разницу в дни
+  let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  return daysDifference;
+};
+
+// const orderDetails = {
+//   discount: props.data.discount,
+//   pay_status: props.data.pay_status,
+//   order_payed:  props.data.order_payed,
+// };
+// console.log(props.data.discount)
 </script>
 
 <template>
-    <ul><Cell v-for="(value, key, index) in props.data" :key="index" :content="value" :class="key"/></ul>
+  <ul class="row">
+    <Cell
+      v-for="(value, key, index) in props.data"
+      :key="index"
+      :content="value"
+      :class="key"
+    />
+    <ul
+    class="cell__column_pay"
+  >
+    <li class="cell">
+      <p class="text">{{ props.data.discount }}</p>
+    </li>
+    <li class="cell">
+      <p class="text">{{ props.data.pay_status }}</p>
+    </li>
+    <li class="cell">
+      <p class="text">{{ props.data.order_payed }}</p>
+    </li>
+  </ul>
+    <li v-if="remainingTime(props.data.dates[0].end_date) < 0" class="cell">
+      <p class="text">
+        Завершилось {{ Math.abs(remainingTime(props.data.dates[0].end_date)) }} дней назад
+      </p>
+    </li>
+    <li v-else class="cell">
+      <p class="text">
+        заканчивается через
+        {{ remainingTime(props.data.dates[0].end_date) }} дней
+      </p>
+    </li>
+  </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  list-style: none;
+}
+</style>
